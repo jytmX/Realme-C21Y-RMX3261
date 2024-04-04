673f50408679668f53747ba0a5278761cf91099e
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-04-04 12:23:32 +0300

    Update twrp_RMX3261.mk

--- twrp_RMX3261.mk
+++ twrp_RMX3261.mk
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 #INHERIT TWRP
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from RMX3261 device
 $(call inherit-product, device/realme/RMX3261/device.mk)
