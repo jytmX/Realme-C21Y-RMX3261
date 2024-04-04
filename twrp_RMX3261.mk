9ae90874d2a990f1251cbbdd99472ed086cf084e
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-04-04 11:26:46 +0300

    Add files via upload

---
+++ twrp_RMX3261.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+#INHERIT GSI KEYS
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+#INHERIT TWRP
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from RMX3261 device
+$(call inherit-product, device/realme/RMX3261/device.mk)
+
+PRODUCT_DEVICE := RMX3261
+PRODUCT_NAME := twrp_RMX3261
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX3261
+PRODUCT_MANUFACTURER := realme
+PRODUCT_RELEASE_NAME := RMX3261
